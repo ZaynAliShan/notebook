b4c89930283647519d69a6ba93770e9dc9527399
zzaynAli <bcsf19a022@pucit.edu.pk>
2022-10-22 22:08:56 +0500

    Connection to Mongo Successul, App is listening at through Express Server and / this route has been tested and hit successfully through thunder client!

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,25 @@
 // entry point for backend
+//1. first we want to connect to our database using mongoose - refer to db.js
+
+//================================================================
+// import connect_to_mongodb
+const connect_to_mongodb = require('./db')
+
+connect_to_mongodb();
+
+// establish connection to express server
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => { // to hit this route we do : http://127.0.0.1:3000/
+  res.send('Hello Zain, Welcome!')
+})
+
+app.listen(port, () => {
+  console.log(`Your app listening on port ${port}`)
+})
+
 
 
 
